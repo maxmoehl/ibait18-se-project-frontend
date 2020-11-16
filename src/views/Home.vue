b28550e77ffbd86ea9d3821de091b94b6d837a28
Maximilian Moehl <maximilian.moehl@sap.com>
2020-11-16 11:06:04 +0100

    add more text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="home">
+    <div class="header">
+      <div class="md-title">
+        Fragen?
+      </div>
+      <p>
+        Bei Fragen zu dieser Anwendung, dem Reservierungsprozess oder Ihrem Besuch bei uns, nutzen Sie bitte die
+        Informationsmöglichkeit in unseren <a href="/faq">Häufig gestellten Fragen (FAQ)</a>.
+      </p>
+    </div>
     <TimeSlotSelection></TimeSlotSelection>
   </div>
 </template>
